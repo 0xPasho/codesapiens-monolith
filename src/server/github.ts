e2016bd947dce2fc6d41432e9abe9d77bd8914e6
Brandon Ag <brandon.aguilarc@gmail.com>
2023-10-24 07:18:08 -0600

    fixiano

--- src/server/github.ts
+++ src/server/github.ts
@@ -2,8 +2,9 @@ import fs from "fs";
 import { createAppAuth } from "@octokit/auth-app";
 import { Octokit } from "@octokit/rest";
 import { env } from "~/env.mjs";
-
-const privateKey = fs.readFileSync("github-app-private-key-prod.pem", "utf-8");
+import path from "path";
+const keyPath = path.join(__dirname, "github-app-private-key-prod.pem");
+const privateKey = fs.readFileSync(keyPath, "utf-8");
 
 const appId =
   typeof env.GITHUB_APP_ID_NUMBER === "number"
