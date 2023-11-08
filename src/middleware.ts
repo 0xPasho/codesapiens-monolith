17b9e924d7d44f660b28dbb867c2d2eb55ab9084
Brandon Ag <brandon.aguilarc@gmail.com>
2023-11-08 00:40:58 -0600

    Change middleware

---
+++ src/middleware.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { env } from "./env.mjs";
+
+async function middleware(request: NextRequest) {
+  // We can't import next-auth here because it's not available in the serverless environment
+  // and it was causing issues, so we rely on the cookie directly
+  const sessionToken = request.cookies.get("next-auth.session-token");
+
+  const isAuthPage =
+    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/register");
+
+  if (!sessionToken) {
+    let from = request.nextUrl.pathname;
+    if (request.nextUrl.search) {
+      from += request.nextUrl.search;
+    }
+    const absoluteFrom = `${env.NEXT_PUBLIC_APP_URL}/${from}`;
+    if (!isAuthPage) {
+      return NextResponse.redirect(
+        `${env.NEXT_PUBLIC_APP_URL}/login?from=${encodeURIComponent(
+          absoluteFrom,
+        )}`,
+      );
+    }
+  } else if (isAuthPage) {
+    return NextResponse.redirect(`${env.NEXT_PUBLIC_APP_URL}/dashboard`);
+  }
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-pathname", request.nextUrl.pathname);
+
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
+
+export default middleware;
+
+export const config = {
+  matcher: [
+    "/dashboard",
+    "/org/:path*",
+    "/account/:path*",
+    "/login",
+    "/register",
+  ],
+};
