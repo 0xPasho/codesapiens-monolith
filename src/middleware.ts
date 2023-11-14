95b8d23be24a677da483abe87f56f46c8a896b29
Brandon Ag <brandon.aguilarc@gmail.com>
2023-11-14 01:49:52 -0600

    keep logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,27 +4,27 @@ import { env } from "./env.mjs";
 async function middleware(request: NextRequest) {
   // We can't import next-auth here because it's not available in the serverless environment
   // and it was causing issues, so we rely on the cookie directly
-  // const sessionToken = request.cookies.get("next-auth.session-token");
+  const sessionToken = request.cookies.get("next-auth.session-token");
 
-  // const isAuthPage =
-  //   request.nextUrl.pathname.startsWith("/login") ||
-  //   request.nextUrl.pathname.startsWith("/register");
+  const isAuthPage =
+    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/register");
 
-  // if (!sessionToken) {
-  //   let from = request.nextUrl.pathname;
-  //   if (request.nextUrl.search) {
-  //     from += request.nextUrl.search;
-  //   }
-  //   const absoluteFrom = `${env.NEXT_PUBLIC_APP_URL}/${from}`;
-  //   if (!isAuthPage) {
-  //     return NextResponse.redirect(
-  //       `${env.NEXT_PUBLIC_APP_URL}/login?from=${encodeURIComponent(
-  //         absoluteFrom,
-  //       )}`,
-  //     );
-  //   }
-  // } else if (isAuthPage) {
-  //   return NextResponse.redirect(`${env.NEXT_PUBLIC_APP_URL}/dashboard`);
+  if (!sessionToken) {
+    let from = request.nextUrl.pathname;
+    if (request.nextUrl.search) {
+      from += request.nextUrl.search;
+    }
+    const absoluteFrom = `${env.NEXT_PUBLIC_APP_URL}/${from}`;
+    if (!isAuthPage) {
+      return NextResponse.redirect(
+        `${env.NEXT_PUBLIC_APP_URL}/login?from=${encodeURIComponent(
+          absoluteFrom,
+        )}`,
+      );
+    }
+  } else if (isAuthPage) {
+    return NextResponse.redirect(`${env.NEXT_PUBLIC_APP_URL}/dashboard`);
   // }
 
   const requestHeaders = new Headers(request.headers);
@@ -39,12 +39,12 @@ async function middleware(request: NextRequest) {
 
 export default middleware;
 
-// export const config = {
-//   matcher: [
-//     "/dashboard",
-//     "/org/:path*",
-//     "/account/:path*",
-//     "/login",
-//     "/register",
-//   ],
-// }
+export const config = {
+  matcher: [
+    "/dashboard",
+    "/org/:path*",
+    "/account/:path*",
+    "/login",
+    "/register",
+  ],
+}
