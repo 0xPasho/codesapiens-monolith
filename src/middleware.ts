2683b841dcbc0977ff8471c19f22afd0f77f4f7a
Brandon Ag <brandon.aguilarc@gmail.com>
2023-11-14 01:06:22 -0600

    chnages

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,4 +47,4 @@ export default middleware;
 //     "/login",
 //     "/register",
 //   ],
-// };
+// }
--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -6,10 +6,7 @@ import { type AppRouter } from "~/server/api/root";
 export const transformer = superjson;
 
 function getBaseUrl() {
-  // if (typeof window !== "undefined") return "";
-  // const vc = process.env.VERCEL_URL;
-  // if (vc) return "https://" + vc;
-  return "http://localhost:3000";
+  return process.env.NEXTAUTH_URL || "http://localhost:3000";
 }
 
 export function getUrl() {
