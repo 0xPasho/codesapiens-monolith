7c6b02216e0022e8bbbeab8b2fd9762bbd788248
Brandon Ag <brandon.aguilarc@gmail.com>
2024-01-15 21:09:11 -0600

    changing middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,31 +6,38 @@ async function middleware(request: NextRequest) {
   // and it was causing issues, so we rely on the cookie directly
   const sessionToken = request.cookies.get("next-auth.session-token");
 
-  // const isAuthPage =
-  //   request.nextUrl.pathname.startsWith("/login") ||
-  //   request.nextUrl.pathname.startsWith("/register");
+  const isAuthPage =
+    request.nextUrl.pathname.startsWith("/login") ||
+    request.nextUrl.pathname.startsWith("/register");
 
-  // if (!sessionToken) {
-  //   let from = request.nextUrl.pathname;
-  //   if (request.nextUrl.search) {
-  //     from += request.nextUrl.search;
-  //   }
-  //   const absoluteFrom = `${env.NEXT_PUBLIC_APP_URL}/${from}`;
-  //   if (!isAuthPage) {
-  //     return NextResponse.redirect(
-  //       `${env.NEXT_PUBLIC_APP_URL}/login?from=${encodeURIComponent(
-  //         absoluteFrom,
-  //       )}`,
-  //     );
-  //   }
-  // } else if (isAuthPage) {
-  //   const urlSearchParams = new URLSearchParams(request.nextUrl.search);
-  //   const fromParam = urlSearchParams.get("from");
-  //   if (fromParam) {
-  //     return NextResponse.redirect(`${env.NEXT_PUBLIC_APP_URL}${fromParam}`);
-  //   }
-  //   return NextResponse.redirect(`${env.NEXT_PUBLIC_APP_URL}/dashboard`);
-  // }
+  console.log({ isAuthPage, sessionToken });
+  if (!sessionToken) {
+    let from = request.nextUrl.pathname;
+    if (request.nextUrl.search) {
+      from += request.nextUrl.search;
+    }
+
+    console.log({ from });
+
+    const absoluteFrom = `${env.NEXT_PUBLIC_APP_URL}/${from}`;
+    if (!isAuthPage) {
+      return NextResponse.redirect(
+        `${env.NEXT_PUBLIC_APP_URL}/login?from=${encodeURIComponent(
+          absoluteFrom,
+        )}`,
+      );
+    }
+  } else if (isAuthPage) {
+    const urlSearchParams = new URLSearchParams(request.nextUrl.search);
+    const fromParam = urlSearchParams.get("from");
+    console.log({ fromv2: fromParam });
+    if (fromParam) {
+      return NextResponse.redirect(`${env.NEXT_PUBLIC_APP_URL}${fromParam}`);
+    }
+    return NextResponse.redirect(`${env.NEXT_PUBLIC_APP_URL}/dashboard`);
+  }
+
+  console.log({ passed: request.nextUrl.pathname });
 
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-pathname", request.nextUrl.pathname);
@@ -46,7 +53,7 @@ export default middleware;
 
 export const config = {
   matcher: [
-    "/dashboard",
+    // "/dashboard",
     "/org/:path*",
     "/account/:path*",
     "/login",
